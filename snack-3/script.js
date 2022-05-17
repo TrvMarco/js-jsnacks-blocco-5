78473c7558c265fd2710b4b2dcaff89a271a7364
TrvMarco <trevisanello.marco@gmail.com>
2022-05-17 12:34:31 +0200

    creato array auto

---
+++ snack-3/script.js
@@ -0,0 +1,53 @@
+const cars = [
+    {
+        marca: 'fiat',
+        modello: 'punto',
+        alimentazione: 'metano'
+    },
+    {
+        marca: 'volkswagen',
+        modello: 'polo',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'fiat',
+        modello: 'panda',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'audi',
+        modello: 'a3',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'mercedes',
+        modello: 'classe a',
+        alimentazione: 'benzina'
+    },
+    {
+        marca: 'volkswagen',
+        modello: 'golf',
+        alimentazione: 'diesel'
+    },
+    {
+        marca: 'fiat',
+        modello: 'punto',
+        alimentazione: 'gpl'
+    },
+    {
+        marca: 'volkswagen',
+        modello: 'id 3',
+        alimentazione: 'elettrico'
+    },
+    {
+        marca: 'suzuki',
+        modello: 'swift',
+        alimentazione: 'elettrico'
+    },
+    {
+        marca: 'kia',
+        modello: 'sportage',
+        alimentazione: 'gpl'
+    },
+
+]
